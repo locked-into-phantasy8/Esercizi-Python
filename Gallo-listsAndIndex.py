5da6925053f8b4550ba4fc0512ca73293f249199
locked-into-phantasy8 <lexy.tyler864@gmail.com>
2024-11-24 10:33:38 +0100

    Update Gallo-listsAndIndex.py
    Impossibile aggiungere istruzioni da eseguire corrispondenti alla condizione "else" senza che collidano con le istruzioni espresse nella condizione "if". Ex: un sostantivo di cui caratteristiche corrispondono alle condizioni espresse in "if" darebbe l'output espresso in "else".

--- Gallo-listsAndIndex.py
+++ Gallo-listsAndIndex.py
@@ -1,9 +1,10 @@
-sostantivo = str(input("Inserisci qui un sostantivo (tutte minuscole) per scoprirne il genere! "))
-e_femminili = ["luce", "colazione","nave"]
-e_maschili = ["mare", "cuore", "amore","bene","male","calice","fulmine","pane","cane","cellulare","bicchiere","leone"]
-if sostantivo[-1] == "a" or sostantivo in e_femminili: 
+sostantivo = str(input("Inserisci qui un sostantivo singolare (tutte minuscole) per scoprirne il genere! "))
+femminili_particolari = ["luce", "colazione","nave", "eco"]
+maschili_particolari = ["mare", "cuore", "amore","bene","male","calice","fulmine","pane","cane","cellulare","bicchiere","leone", "papa", "papà"]
+consonanti = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x","z" ]
+if sostantivo[-4:] == "ista" and sostantivo[-5:] != "vista" and len(sostantivo) > 5:
+    print("Questo sostantivo può essere sia maschile che femminile in base al contesto.")
+elif sostantivo[-1] == "a" or sostantivo[-1] == "à" and sostantivo not in maschili_particolari or sostantivo[-1] == "i" or sostantivo in femminili_particolari: 
     print("Femminile")
-elif sostantivo[-1] == "o" or sostantivo in e_maschili:
+elif sostantivo[-1] == "o" or sostantivo[-1] in consonanti or sostantivo in maschili_particolari: 
     print ("Maschile")
-else:
-  print("Ci dispiace, non conosciamo ancora il genere di questo sostantivo.")
